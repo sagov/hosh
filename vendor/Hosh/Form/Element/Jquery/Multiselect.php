adbc0ba7498b6de5d3ef87ef4e3a1e2b4fe80e19
Sagov <vladimir-sagov@yandex.ru>
2016-10-25 10:47:38 +0300

    jquery_multiselect

--- vendor/Hosh/Form/Element/Jquery/Multiselect.php
+++ vendor/Hosh/Form/Element/Jquery/Multiselect.php
@@ -23,7 +23,8 @@ require_once 'Zend/Form/Element/Multiselect.php';
 class Hosh_Form_Element_Jquery_Multiselect extends Zend_Form_Element_Multiselect
 {
 
-    protected $_isbtn = false;
+    protected $_isbtn = true;
+    protected $_issearch = true;
 
     public function init ()
     {
@@ -34,34 +35,88 @@ class Hosh_Form_Element_Jquery_Multiselect extends Zend_Form_Element_Multiselect
         $pattern = $form->getPattern();
         $patten_element = $pattern->getElement($name);
         
-        $param = array();
+        $param = $afterInit = array();
         
         $view = Hosh_View::getInstance();
+        $view->Font_Fontawesome();
         $view->JQuery_Multiselect();
+        
         $view->AddStyleSheet(
                 '/libraries/hosh/Element/jquery/multiselect/style.css');
         
-        $isbtn = (boolean)($patten_element->get('isbtn', $this->_isbtn));
+        $issearch = (boolean) ($patten_element->get('issearch', 
+                $this->_issearch));
+        if ($issearch) {
+            $view->JQuery_Quicksearch();
+            $param['selectableHeader'] = 'selectableHeader: ' .
+                     Zend_Json::encode(
+                            '<input type="text" class="form-control search-input" placeholder="search..." autocomplete="off">');
+            $param['selectionHeader'] = 'selectionHeader: ' .
+                     Zend_Json::encode(
+                            '<input type="text" class="form-control search-input" placeholder="search..." autocomplete="off">');
+            $afterInit[] = '
+                                   var that = this,
+            $selectableSearch = that.$selectableUl.prev(),
+            $selectionSearch = that.$selectionUl.prev(),
+            selectableSearchString = \'#\'+that.$container.attr("id")+\' .ms-elem-selectable:not(.ms-selected)\',
+            selectionSearchString = \'#\'+that.$container.attr("id")+\' .ms-elem-selection.ms-selected\';
+
+            that.qs1 = $selectableSearch.quicksearch(selectableSearchString)
+            .on("keydown", function(e){
+              if (e.which === 40){
+                that.$selectableUl.focus();
+                return false;
+              }
+            });
+        
+            that.qs2 = $selectionSearch.quicksearch(selectionSearchString)
+            .on("keydown", function(e){
+              if (e.which == 40){
+                that.$selectionUl.focus();
+                return false;
+              }
+          
+                                 });';
+            $param['afterSelect'] = 'afterSelect: function(){
+                                this.qs1.cache();
+                                this.qs2.cache();
+                            }';
+            $param['afterDeselect'] = 'afterDeselect: function(){
+                                this.qs1.cache();
+                                this.qs2.cache();
+                                }';
+        }
+        
+        $isbtn = (boolean) ($patten_element->get('isbtn', $this->_isbtn));
         if ($isbtn) {
-            $param['selectableFooter'] = 'selectableFooter: '.Zend_Json::encode('<button class="btn btn-default ms-footer-btn" data-task="ms-select-all"><i class="fa fa-check" aria-hidden="true"></i> выбрать все</button>');
-            $param['selectionFooter'] = 'selectionFooter:'.Zend_Json::encode('<button class="btn btn-default ms-footer-btn" data-task="ms-deselect-all"><i class="fa fa-trash" aria-hidden="true"></i> очистить</button>');
-            $param['afterInit'] = 'afterInit:function(ms){
-                                    var that = this;
+            $param['selectableFooter'] = 'selectableFooter: ' .
+                     Zend_Json::encode(
+                            '<button class="btn btn-default ms-footer-btn" data-task="ms-select-all"><i class="fa fa-check" aria-hidden="true"></i> выбрать все</button>');
+            $param['selectionFooter'] = 'selectionFooter:' .
+                     Zend_Json::encode(
+                            '<button class="btn btn-default ms-footer-btn" data-task="ms-deselect-all"><i class="fa fa-trash" aria-hidden="true"></i> очистить</button>');
+            $afterInit[] = '
+                                   var that = this;
                                    ms.on("click","[data-task=ms-select-all]",function(){
                                 		that.select_all();
                                 		return false;
                                 	}).on("click","[data-task=ms-deselect-all]",function(){
                                 		that.deselect_all();
                                 		return false;
-                                	});
-                                    
-                                 }';
+                                	});                                    
+                                 ';
+        }
+        
+        if (count($afterInit) > 0) {
+            $param['afterInit'] = 'afterInit:function(ms){' .
+                     implode('', $afterInit) . '}';
         }
         
         $script = '
 					;(function($) {
 					   $(document).ready(function() {
-                           $("#' . $idelement . '").multiSelect({'.implode(",",$param).'});                            
+                           $("#' . $idelement . '").multiSelect({' .
+                 implode(",", $param) . '});                            
                                 
 		               });
 		            })(jQuery);';
